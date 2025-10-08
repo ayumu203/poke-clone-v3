127a9f47fd1a1e5cb9620414a3169f346bb26655
arm203 <ochiohita@gmail.com>
2025-10-09 00:14:32 +0900

    fix: serverリファクタリングのための整理

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0cd999b649d80abd1ae78860381ca11c6c7e42e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+142b240f59b819299cd5ceb7e78cd8f0b4e6c979")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
